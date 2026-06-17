bc9eef2bdc1fdf3f4c6943c153dc24d01ea48ae3
Bunny <bunny202410@gmail.com>
2026-06-18 00:51:45 +0530

    Remove old/unnecessary files

--- Lisa version0.1/musicLibrary.py
+++
@@ -1,4 +0,0 @@
-music={
-    "music":"https://www.youtube.com/watch?v=JOX09U8noOE"
-}
-
---
+++ main.py
@@ -0,0 +1,136 @@
+import os
+# import google.generativeai as genai
+import speech_recognition as sr
+import pyttsx3
+import requests
+import webbrowser
+from playsound import playsound
+import datetime
+engine = pyttsx3.init()
+
+def speak(text):
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id if len(voices) > 1 else voices[0].id)  # Fallback to voices[0] if [1] doesn't exist
+    engine.say(text)
+    engine.runAndWait()
+
+# def news():
+#     url ="your news url with api hidden for security reason"
+#     response = requests.get(url)
+    
+#     if response.status_code == 200:
+#         data = response.json()
+#         titles = [article['title'] for article in data['articles']]
+#         top_three_titles = titles[:3]
+#         formatted_titles = "\n".join([f"{i + 1}. {title}" for i, title in enumerate(top_three_titles)])
+#         speak("Sure, here are the top 3 news headlines.")
+#         speak(formatted_titles)
+#     else:
+#         print("No news right now.")
+
+def greet_based_on_time():
+    current_time = datetime.datetime.now().time()
+    if current_time < datetime.time(12, 0):
+        greeting = "Good Morning Sir"
+    elif datetime.time(12, 0) <= current_time < datetime.time(18, 0):
+        greeting = "Good Afternoon Sir"
+    else:
+        greeting = "Good Evening Sir"
+    
+    speak(greeting)
+
+
+def search(command):
+    to_remove = "search "
+    if command.startswith(to_remove):
+        command = command[len(to_remove):]
+        app = command
+    else:
+        app = command.split(" ")[1]
+    link = "https://www.google.co.in/search?q=" + app
+    playsound("asset/query.mp3")
+    webbrowser.open(link)
+
+# def site(command):   
+#     app = command.split(" ")[1]
+#     links = {
+#         "youtube": "https://www.youtube.com/",
+#         "facebook": "https://www.facebook.com/",
+#         "spotify": "https://open.spotify.com/",
+#         "instagram": "https://www.instagram.com/",
+#         "twitter": "https://www.x.com/"
+#     }
+#     link = links.get(app, f"https://www.{app}")
+#     playsound("asset\query.mp3")
+#     webbrowser.open(link)
+
+
+# def ai_search(command):
+#     my_api_key ="here will be the key hidden for security reson " 
+#     genai.configure(api_key=my_api_key)
+#     generation_config = {
+#         "temperature": 0.7,  # Lower value for more focused answers
+#         "top_p": 0.9,
+#         "top_k": 40,
+#         "max_output_tokens": 50,  # Lower value to restrict response length
+#         "response_mime_type": "text/plain",
+#     }
+#     model = genai.GenerativeModel(
+#     model_name="gemini-1.5-flash",
+#     generation_config=generation_config,
+#     # safety_settings = Adjust safety settings
+#     # See https://ai.google.dev/gemini-api/docs/safety-settings
+#     )
+
+#     chat_session = model.start_chat(
+#     history=[
+#     ]
+#     )
+
+#     response = chat_session.send_message(command)
+#     playsound("D:\\project\\query.mp3")
+#     speak(response.text)
+
+
+def mainf(r):
+    with sr.Microphone() as source:
+        # audio = r.listen(source, timeout=None, phrase_time_limit=10)
+        # word = r.recognize_google(audio).lower()
+        
+        # if "alexa" in word:
+        while (1):
+            playsound("asset/query.mp3")
+            speak("Hey there! I am Lisa, your virtual assistant. How can I help you?")
+            while True:
+                print("Listening...")
+                try:
+                    audio = r.listen(source, timeout=5, phrase_time_limit=3)
+                    command = r.recognize_google(audio).lower()
+                    if "exit" in command:
+                        speak("Thank you")
+                        playsound("asset/off.mp3")
+                        exit()
+                    # elif "news" in command:
+                    #     playsound("asset\query.mp3")
+                    #     news()
+                    # elif "play music" in command:
+                    #     operation(command)
+                    elif "search" in command:
+                        search(command)
+                    # elif "open" in command:
+                    #     site(command)
+                    else:
+                        # ai_search(command)
+                        print("hello")
+                except sr.UnknownValueError:
+                    print("Couldn't understand the audio. Please try again.")
+                except sr.RequestError as e:
+                    print(f"Error with the Google service: {e}")
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    playsound("asset/start.mp3")
+    r = sr.Recognizer()
+    while True:
+        mainf(r)
